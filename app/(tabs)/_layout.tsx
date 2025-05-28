8bb051d53ba945906127d3f866693e7d018d2d51
JesusPachdez <pacheco1290@gmail.com>
2025-05-28 17:42:50 -0600

    Initial commit: Coffee shop app with home and contact screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarInactiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        // headerTitleAlign: "center",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <IconSymbol
+              size={28}
+              name={focused ? "house.fill" : "house"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: "Contact",
+          tabBarIcon: ({ color, focused }) => (
+            <IconSymbol
+              size={28}
+              name={focused ? "person.fill" : "person"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
